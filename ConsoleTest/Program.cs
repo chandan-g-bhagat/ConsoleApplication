06135ae7770caac82b0ffe3805a4c32da85d8055
speedchandu <chandan.bhagat@outlook.com>
2016-04-29 08:40:26 +0545

    Initial Commit.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLibrary;
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Testmessage();
+            Console.ReadLine();
+        }
+
+        static void Testmessage()
+        {
+            Test t = new Test();
+            Console.WriteLine("From T Object");
+           // Console.WriteLine("From Static "+Test.TestFunction());
+            Console.WriteLine("From non static "+t.TestNonStatic());
+            Test t1 = new Test();
+
+            Console.WriteLine("From t1 object");
+            //Console.WriteLine("From Static " + Test.TestFunction());
+            Console.WriteLine("From non static " + t1.TestNonStatic());
+
+        }
+    }
+}
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class Test
+    {
+        private static int i = 10;
+
+        private int j=10;
+        public static string TestFunction()
+        {
+            return i.ToString();
+        }
+
+        public string TestNonStatic()
+        {
+            i++;
+            j++;
+            return j.ToString();
+        }
+    }
+}
